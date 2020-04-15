ee380e224df1781ae42e23b00ae5acec0c93bec1
Scarlet <developerscarlet@gmail.com>
2020-04-14 21:21:19 -0400

    Added Navigation class.

---
+++ src/__test__/navigation.test.tsx
@@ -0,0 +1,8 @@
+import NavigationComponents from '../lib/Navigation/Navigation';
+
+describe('NavigationComponent', () => {
+    it('is truthy', () => {
+      expect(NavigationComponents).toBeTruthy()
+    })
+  })
+  
