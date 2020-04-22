84c7a538dbd399331f1ec1d22bf6513e6aef8a7e
Akane <akane.simpson@gmail.com>
2020-04-21 23:35:20 -0400

    Added webpack config settings to main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,43 @@
+const path = require('path');
+
 module.exports = {
-    stories: ['../src/stories/**/*/stories.tsx'],
-    addons: [
-        '@storybook/preset-create-react-app',
-        '@storybook/addon-links',
-        '@storybook/addon-actions'
-    ]
-}
+  stories: ['../src/**/*.stories.(ts|tsx|js|jsx)'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    {
+      name:"@storybook/addon-docs",
+      options: {
+        configureJSX: true
+      }
+    }
+  ],
+
+  webpackFinal: async config => {
+    config.module.rules.push(
+      {
+      test: /\.(ts|tsx|js|jsx)$/,
+      include: path.resolve(__dirname, "../src"),
+      use:[
+        require.resolve("ts-loader"),
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+          options: {
+            tsconfigPath: path.resolve(__dirname, '../tsconfig.json'),
+          },
+        },
+      ],
+      },
+      { 
+      test: /\.s[c|a]ss?$/,
+      use:[
+          { loader: require.resolve("style-loader") },
+          { loader: require.resolve('css-loader') },
+          { loader: require.resolve('sass-loader') },
+      ] 
+  });
+    config.resolve.extensions.push('.ts', '.tsx', '.js', '.jsx')
+    return config;
+  },
+};
