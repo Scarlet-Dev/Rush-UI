fa9033300d0150df376d68db764f6e74a6ecd8c8
Scarlet <developerscarlet@gmail.com>
2020-05-25 22:28:41 -0400

    File restructuring for From file. Moved from class to func component.

--- src/lib/form/form.tsx
+++ src/lib/form/form.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './_form.scss';
 
 export interface FormProps{
     id?: string;
@@ -20,105 +21,81 @@ export interface FormProps{
 
 
 export function Input(props: FormProps): any{
-     * 
-     * @param props
-     * 
-     * @method handleChange - Used 
-     * @property props.id (The id assigned to input)
-     * @property props.type - The type of input being called
-     * @property props.placeholder
-     * @property props.pattern
-     */
-        // let handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
-        //     let inputValue = event.target.value;
-        // }
-        // @property 
-        switch (props.type) {
-            case 'text':
-                return(
-                    <input type={props.type} id={props.id} minLength={props.min || 1} maxLength={props.max || 100} 
-                    placeholder={props.placeholder || ""}/>
-                );
-            case 'range':
-            case 'number':
-                return(
-                    <input type={props.type} id={props.id} min={props.min || 1} max={props.max || 100} 
-                    step={props.step || 1} />
-                );
-            case 'date':
-            case 'datetime-local':
-            case 'month':
-            case 'week':
-                return(
-                    <input type={props.type} id={props.id} min={props.main || ""} max={props.max || ""}/>
-                );
-            case 'email':
-            case 'tel':
-                return(
-                    <input type={props.type} id={props.id} pattern={props.pattern || "[0-9]{3}-[0-9]{3}-[0-9]{4}"} 
-                    placeholder={props.placeholder || ""}/>
-                );
-            case 'password':
-            case 'radio':
-            case 'checkbox':
-            case 'file':
-            case 'reset':
-            case 'url':
-                default:
-                return(
-                    <input type={props.type} id={props.id}/>
-                );
-        }
+    switch (props.type) {
+        case 'text':
+            return(
+                <input type={props.type} id={props.id} minLength={props.min} maxLength={props.max} 
+                placeholder={props.placeholder}/>
+            );
+        case 'range':
+        case 'number':
+            return(
+                <input type={props.type} id={props.id} min={props.min || 1} max={props.max || 100} 
+                step={props.step} />
+            );
+        case 'date':
+        case 'datetime-local':
+        case 'month':
+        case 'week':
+            return(
+                <input type={props.type} id={props.id} min={props.min} max={props.max}/>
+            );
+        case 'email':
+        case 'tel':
+            return(
+                <input type={props.type} id={props.id} pattern={props.pattern} 
+                placeholder={props.placeholder}/>
+            );
+        case 'password':
+        case 'radio':
+        case 'checkbox':
+        case 'file':
+        case 'reset':
+        case 'url':
+            default:
+            return(
+                <input type={props.type} id={props.id}/>
+            );
     }
+}
 
 
 export function Label(props: FormProps){
-     * 
-     * @param props 
-     * @property props.id
-     * @property props.for
-     * @property props.text
-     */
-        return(
-            <label id={props.id} htmlFor={props.for}>
-                {props.text}
-            </label>
-        )
-    }
+    return(
+        <label id={props.id} htmlFor={props.for}>
+            {props.text}
+        </label>
+    )
+}
 
 export function TextArea(props: FormProps){
-     * 
-     * @param props
-     * @property props.id 
-     * @property props.rows 
-     * @property props.cols 
-     * @property props.placeholder 
-     */
-        return(
-            <textarea id={props.id} rows={props.row || 4} cols={props.cols || 50} placeholder={props.placeholder || ""}></textarea>
-        )
-    }
+    return(
+        <textarea id={props.id} rows={props.rows} cols={props.cols} placeholder={props.placeholder}></textarea>
+    )
+}
 
-    /**
+
 export function Dropdown(props: FormProps){
-     * @param props 
-     */
 
-        let handleChange = (event: React.ChangeEvent<HTMLSelectElement>) =>{
-            let selectedValue = event.target.value;
-            props.onSelectChange(selectedValue);
-        }
-
-        let arrayOfData = props.arrayOfData;
-        let options: React.FormEvent<HTMLOptionElement>[] = arrayOfData.map((data: any) =>{
-            <option key={data.id} value={data.value}>
-                {data.name}
-            </option>
-        });
-
-        return(
-            <select id={props.id} onChange={handleChange}>
-                {options}
-            </select>
-        )
+    let handleChange = (event: React.ChangeEvent<HTMLSelectElement>) =>{
+        let selectedValue = event.target.value;
+        props.onSelectChange(selectedValue);
     }
+
+    let arrayOfData = props.dataArray;
+    let options = arrayOfData.map(
+        (data: any) =>
+        {
+            return(
+                <option key={data.id} value={data.value}>
+                {data.name}
+                </option>
+            )
+    });
+
+    return(
+        <select id={props.id} onChange={handleChange}>
+            {options}
+        </select>
+    )
+}
