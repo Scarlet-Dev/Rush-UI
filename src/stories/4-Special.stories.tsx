b00763176f6a8dfdfeb360466b0ef01bfc74f3b9
Scarlet <developerscarlet@gmail.com>
2020-06-09 21:54:44 -0400

    Add stories file for Special Components

---
+++ src/stories/4-Special.stories.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as SpecialComponent from "../components/special"
+
+export default{
+    title: '',
+    component: SpecialComponent,
+}
+
+export const Elem1 = () => <SpecialComponent.Tile id='test'></SpecialComponent.Tile>
