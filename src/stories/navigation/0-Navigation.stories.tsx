e61807d7101e324c84a63379905295f9b3087eda
Scarlet <developerscarlet@gmail.com>
2020-06-21 01:18:32 -0400

    Add Navigation story

---
+++ src/stories/navigation/0-Navigation.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const NavigationPage = () => {
+    return (
+        <>
+            <h1>Navigation Page</h1>
+        </>
+    )
+}
+
+export default {
+    title: 'Rush UI | Navigation Components',
+    component: NavigationPage
+}
