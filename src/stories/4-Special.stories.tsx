7f8655d419217ef36bc3c8f01b7811c2b60d7110
Scarlet <developerscarlet@gmail.com>
2020-06-20 23:03:47 -0400

    Delete UI storeis

--- src/stories/4-Special.stories.tsx
+++
@@ -1,9 +0,0 @@
-// import * as React from 'react';
-// import * as SpecialComponent from "../components/special"
-
-// export default{
-//     title: 'RushUI | Special Components and Elements',
-//     component: SpecialComponent,
-// }
-
-// export const Elem1 = () => <SpecialComponent.Tile id='test'></SpecialComponent.Tile>
