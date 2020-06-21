140bf42c7c961ce889a8ec64c39459c1a2b43d0b
Scarlet <developerscarlet@gmail.com>
2020-06-21 01:19:28 -0400

    Add Card story to General Comps

---
+++ src/stories/general/2-Card.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { ElementFabricatory } from '../../components/index'
+import { GeneralProps } from "../../components/baseComponents";
+
+const compCategory = 'general'
+const compType = 'card'
+var cardType: GeneralProps = {}
+
+const elFab = new ElementFabricatory();
+const BasicCards = () => {return elFab.createElement(compCategory, compType, cardType)}
+
+storiesOf('Rush UI/General Components/Cards', module)
+.add('with basic properties', () => <BasicCards />)
