b1bc23b347b86e7220a103c825f75d99ff45c9b6
Scarlet <developerscarlet@gmail.com>
2020-06-21 01:19:58 -0400

    Add Section story to General Comps

---
+++ src/stories/general/6-Section.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { ElementFabricatory } from '../../components/index'
+import { GeneralProps } from "../../components/baseComponents";
+
+const compCategory = 'general'
+const compType = 'section'
+var buttonType: GeneralProps = {}
+
+const elFab = new ElementFabricatory();
+const BasicNavigation = () => { return elFab.createElement(compCategory, compType, buttonType) }
+
+storiesOf('Rush UI/General Components/Sections', module)
+.add('with basic properties', () => <BasicNavigation />)
