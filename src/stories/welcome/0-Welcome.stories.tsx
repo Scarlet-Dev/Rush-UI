05091a020790aed6e55a1910b5db035e4f629580
Scarlet <developerscarlet@gmail.com>
2020-06-21 01:19:05 -0400

    Update Welcom story title

--- src/stories/welcome/0-Welcome.stories.tsx
+++ src/stories/welcome/0-Welcome.stories.tsx
@@ -3,7 +3,7 @@ import { linkTo } from '@storybook/addon-links';
 import { Welcome } from '@storybook/react/demo';
 
 export default {
-  title: 'Welcome',
+  title: 'Rush UI | Welcome',
   component: Welcome,
 };
 
