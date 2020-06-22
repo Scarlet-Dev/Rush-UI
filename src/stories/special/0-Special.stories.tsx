4371122365006780b133ec48137663f41db0407c
Scarlet <developerscarlet@gmail.com>
2020-06-22 10:09:47 -0400

    Add default special page for spec library

---
+++ src/stories/special/0-Special.stories.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+export const SpecialPage = () => {
+    return (
+        <>
+            <h1>Special Page</h1>
+        </>
+    )
+}
+
+export default {
+    title: 'Rush UI | Special Components',
+    component: SpecialPage
+}
